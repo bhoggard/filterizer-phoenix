521c1272042fe6c8410a7e21899ef015c87b8ccc
Barry Hoggard <bhoggard@gmail.com>
2025-08-29 23:16:06 -0400

    clean up layout

--- lib/filterizer_web/components/layouts.ex
+++ lib/filterizer_web/components/layouts.ex
@@ -38,10 +38,10 @@ defmodule FilterizerWeb.Layouts do
     <header class="navbar px-4 sm:px-6 lg:px-8">
       <div class="flex-1">
         <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
+          <span class="text-lg tracking-wide font-semibold">FILTERIZER</span>
         </a>
       </div>
+      <%= if false do %>
       <div class="flex-none">
         <ul class="flex flex-column px-1 space-x-4 items-center">
           <li>
@@ -60,6 +60,7 @@ defmodule FilterizerWeb.Layouts do
           </li>
         </ul>
       </div>
+      <% end %>
     </header>
 
     <main class="px-4 py-20 sm:px-6 lg:px-8">
